62f3f5fdf386f78bced7c109a114d4ce7eb3013a
Pranay Sowdaboina <svenkatapr@walmartlabs.com>
2015-10-12 18:24:09 -0700

    Pull to refresh.

--- app/src/main/java/com/example/spvenka/instagram/FeedActivity.java
+++ app/src/main/java/com/example/spvenka/instagram/FeedActivity.java
@@ -1,5 +1,6 @@
 package com.example.spvenka.instagram;
 
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,22 +15,40 @@ import java.util.ArrayList;
 
 public class FeedActivity extends AppCompatActivity {
 
-    String url = "https://api.instagram.com/v1/media/popular?client_id=0b92f83a8a8743b899b5079a18326c6f";
-    public ArrayList<InstagramPhoto> photos = null;
-    InstagramPhotosAdapter adapter;
+    private String url = "https://api.instagram.com/v1/media/popular?client_id=0b92f83a8a8743b899b5079a18326c6f";
+    private ArrayList<InstagramPhoto> photos = null;
+    private InstagramPhotosAdapter adapter;
+    private SwipeRefreshLayout swipeContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+
+        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // Your code to refresh the list here.
+                // Make sure you call swipeContainer.setRefreshing(false)
+                // once the network request has completed successfully.
+                fetchPopularPhotos(0);
+            }
+        });
+
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
         photos = new ArrayList<>();
         adapter = new InstagramPhotosAdapter(this, photos);
         ListView listView = (ListView) findViewById(R.id.lvPhotos);
         listView.setAdapter(adapter);
-        fetchPopularPhotos();
+        fetchPopularPhotos(0);
     }
 
-    public void fetchPopularPhotos() {
+    public void fetchPopularPhotos(int page) {
         AsyncHttpClient client = new AsyncHttpClient();
         client.get(url, new JsonHttpResponseHandler() {
 
@@ -43,6 +62,7 @@ public class FeedActivity extends AppCompatActivity {
                 super.onSuccess(statusCode, headers, response);
                 Log.i("DEBUG", response.toString());
                 JSONArray photosArray = null;
+                adapter.clear();
                 try {
                     photosArray = response.getJSONArray("data");
                     for (int i = 0; i < photosArray.length(); i++) {
@@ -59,7 +79,9 @@ public class FeedActivity extends AppCompatActivity {
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
+                adapter.addAll(photos);
                 adapter.notifyDataSetChanged();
+                swipeContainer.setRefreshing(false);
             }
 
             @Override
